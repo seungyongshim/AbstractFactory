e11f01b82c934e2d504f60513b75416a1309606b
심승용 <seungyong.shim@mirero.co.kr>
2019-12-23 00:02:38 +0900

    Microsoft.Extensions.DependencyInjection 추가

---
+++ ConsoleMSDI/Program.cs
@@ -0,0 +1,29 @@
+﻿// https://docs.microsoft.com/en-us/archive/msdn-magazine/2016/june/essential-net-dependency-injection-with-net-core
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Lib;
+using LibA;
+using LibB;
+using LibC;
+using LibD;
+
+namespace ConsoleMSDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddTransient<IClassA, ClassA>();
+            serviceCollection.AddTransient<IClassB, ClassB>();
+            serviceCollection.AddTransient<IClassC, ClassC>();
+            serviceCollection.AddTransient<IClassD, ClassD>();
+
+            var service = serviceCollection.BuildServiceProvider();
+            var classA = service.GetService<IClassA>();
+
+            Console.WriteLine(classA);
+        }
+    }
+}
